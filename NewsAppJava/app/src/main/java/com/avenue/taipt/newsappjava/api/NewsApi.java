ddc5a8a2b45308054725f2af08f484bacc498c84
taipham-ave <tai.pham@avenue-net.com>
2021-12-15 16:19:38 +0700

    Change format code of NewsApp

--- NewsAppJava/app/src/main/java/com/avenue/taipt/newsappjava/api/NewsApi.java
+++ NewsAppJava/app/src/main/java/com/avenue/taipt/newsappjava/api/NewsApi.java
@@ -9,8 +9,16 @@ import retrofit2.http.Query;
 public interface NewsApi {
 
     @GET(value = "v2/top-headlines")
-    Response<NewsResponse> getBreakingNews(@Query("country") String countryCode, @Query("page") int pageNumber, @Query("apiKey") String apiKey);
+    Response<NewsResponse> getBreakingNews(
+            @Query("country") String countryCode,
+            @Query("page") int pageNumber,
+            @Query("apiKey") String apiKey
+    );
 
     @GET(value = "v2/everything")
-    Response<NewsResponse> searchForNews(@Query("q") String searchQuery, @Query("page") int pageNumber, @Query("apiKey") String apiKey);
+    Response<NewsResponse> searchForNews(
+            @Query("q") String searchQuery,
+            @Query("page") int pageNumber,
+            @Query("apiKey") String apiKey
+    );
 }
