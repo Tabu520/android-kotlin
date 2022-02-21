c8e5f25281465e5ffaf99e6296dcadbcf91b70ee
TaiPham <tai.pham@avenue-net.com>
2022-02-21 11:30:22 +0700

    Cupcake App: part 1

--- android-basics-kotlin-cupcake-app/app/src/main/java/com/example/cupcake/PickupFragment.kt
+++ android-basics-kotlin-cupcake-app/app/src/main/java/com/example/cupcake/PickupFragment.kt
@@ -21,6 +21,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentPickupBinding
 
 /**
@@ -32,6 +34,7 @@ class PickupFragment : Fragment() {
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
     // when the view hierarchy is attached to the fragment.
     private var binding: FragmentPickupBinding? = null
+    private val sharedViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,6 +49,7 @@ class PickupFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding?.apply {
+            viewModel = sharedViewModel
             nextButton.setOnClickListener { goToNextScreen() }
         }
     }
@@ -55,6 +59,7 @@ class PickupFragment : Fragment() {
      */
     fun goToNextScreen() {
         Toast.makeText(activity, "Next", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_pickupFragment_to_summaryFragment)
     }
 
     /**
