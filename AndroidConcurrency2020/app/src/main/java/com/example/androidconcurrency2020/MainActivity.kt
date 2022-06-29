ddb037e07d0d12c0bbd2a1c7acd26d18ff4d3376
taipham-ave <tai.pham@avenue-net.com>
2022-06-29 15:08:36 +0700

    AndroidConcurrency2020: JobIntentService from IntentService

--- AndroidConcurrency2020/app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
+++ AndroidConcurrency2020/app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
@@ -2,15 +2,14 @@ package com.example.androidconcurrency2020
 
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import android.util.Log
-import android.widget.ImageView
+import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.example.androidconcurrency2020.databinding.ActivityMainBinding
-import kotlin.concurrent.thread
-import kotlin.random.Random
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 const val MESSAGE_KEY = "MESSAGE_KEY"
 const val DICE_NUMBER_KEY = "DICE_NUMBER_KEY"
@@ -20,24 +19,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var mainViewModel: MainViewModel
-    var imageViews = emptyArray<ImageView>()
-    val drawables = arrayOf(
-        R.drawable.dice_1,
-        R.drawable.dice_2,
-        R.drawable.dice_3,
-        R.drawable.dice_4,
-        R.drawable.dice_5,
-        R.drawable.dice_6
-    )
-    private val handler = object : Handler(Looper.getMainLooper()) {
-        override fun handleMessage(msg: Message) {
-            val result = msg.data
-            val dieNumber = result?.getInt(DICE_NUMBER_KEY) ?: 0
-            val dieIndex = result?.getInt(DICE_INDEX_KEY) ?: 1
-            Log.i(LOG_TAG, "index=$dieIndex, value=$dieNumber")
-            imageViews[dieIndex].setImageResource(drawables[dieNumber - 1])
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,20 +27,70 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initView()
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        mainViewModel.dieValue.observe(this) {
-            imageViews[it.first].setImageResource(drawables[it.second - 1])
-        }
         // Initialize button click handlers
-        binding.rollButton.setOnClickListener { mainViewModel.rollTheDice() }
+        with(binding) {
+            runButton.setOnClickListener { runCode() }
+            clearButton.setOnClickListener { clearOutput() }
+        }
 
     }
 
-    private fun initView() {
-        imageViews = arrayOf(binding.die1, binding.die2, binding.die3, binding.die4, binding.die5)
+    /**
+     * Run some code
+     */
+    private fun runCode() {
+//        thread(start = true) {
+//            val bundle = Bundle()
+//            for (i in 1..10) {
+//                bundle.putString(MESSAGE_KEY, "Looping $i")
+//                Message().also {
+//                    it.data = bundle
+//                    handler.sendMessage(it)
+//                }
+//                Thread.sleep(1000)
+//            }
+//            bundle.putString(MESSAGE_KEY, "All done!")
+//            Message().also {
+//                it.data = bundle
+//                handler.sendMessage(it)
+//            }
+//        }
+
+//        CoroutineScope(Dispatchers.Main).launch {
+//            val result = fetchSomething()
+//            log(result)
+//        }
+        MyIntentService.startActionFoo(this, "Param 1", "Param 2")
     }
 
+    /**
+     * Clear log display
+     */
+    private fun clearOutput() {
+        binding.logDisplay.text = ""
+        scrollTextToEnd()
+    }
 
+    /**
+     * Log output to logcat and the screen
+     */
+    @Suppress("SameParameterValue")
+    private fun log(message: String) {
+        Log.i(LOG_TAG, message)
+        binding.logDisplay.append(message + "\n")
+        scrollTextToEnd()
+    }
+
+    /**
+     * Scroll to end. Wrapped in post() function so it's the last thing to happen
+     */
+    private fun scrollTextToEnd() {
+        Handler().post { binding.scrollView.fullScroll(ScrollView.FOCUS_DOWN) }
+    }
+
+    private suspend fun fetchSomething(): String {
+        delay(2000)
+        return "Pham The Tai"
+    }
 
 }
