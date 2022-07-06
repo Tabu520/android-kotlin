712f612d6d7c8f1d9810564aae287513ff6faa67
taipham-ave <tai.pham@avenue-net.com>
2022-07-06 17:07:28 +0700

    Pokedex: Init + Dagger-Hilt Setup

--- Pokedex/app/src/main/java/com/taipt/pokedex/MainActivity.kt
+++ Pokedex/app/src/main/java/com/taipt/pokedex/MainActivity.kt
@@ -3,21 +3,50 @@ package com.taipt.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.taipt.pokedex.ui.theme.PokedexTheme
+import com.taipt.pokedex.utils.Constants
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Constants.POKEMON_LIST_SCREEN
+                ) {
+                    composable(Constants.POKEMON_LIST_SCREEN) {
 
+                    }
+                    composable("${Constants.POKEMON_DETAIL_SCREEN}/{${Constants.DOMINANT_COLOR}}/{${Constants.POKEMON_NAME}}",
+                        arguments = listOf(
+                            navArgument(Constants.DOMINANT_COLOR) {
+                                type = NavType.IntType
+                            },
+                            navArgument(Constants.POKEMON_NAME) {
+                                type = NavType.StringType
+                            }
+                        )
+                    ) {
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt(Constants.DOMINANT_COLOR)
+                            color?.let { Color(it) } ?: Color.White
+                        }
+                        val pokemonName = remember {
+                            it.arguments?.getString(Constants.POKEMON_NAME)
+                        }
+                    }
+                }
             }
         }
     }
---
+++ Pokedex/app/src/main/java/com/taipt/pokedex/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.taipt.pokedex.utils
+
+object Constants {
+
+    const val POKEMON_LIST_SCREEN = "pokemon_list_screen"
+    const val POKEMON_DETAIL_SCREEN = "pokemon_detail_screen"
+    const val DOMINANT_COLOR = "dominantColor"
+    const val POKEMON_NAME = "pokemonName"
+}
