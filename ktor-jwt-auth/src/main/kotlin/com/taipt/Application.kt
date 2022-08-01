732bf9c5830da2b029e28179ddaed8e03bc8901c
taipham-ave <tai.pham@avenue-net.com>
2022-08-01 14:56:48 +0700

    Ktor: init

---
+++ ktor-jwt-auth/src/main/kotlin/com/taipt/Application.kt
@@ -0,0 +1,15 @@
+package com.taipt
+
+import io.ktor.server.application.*
+import com.taipt.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
